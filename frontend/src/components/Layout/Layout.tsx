bd6c0906602065c4ef1f5e221bd923302c484084
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-07-10 13:28:45 +0300

    feat: add sale and rent menu

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
+import './Layout.scss';
 
 export default function Layout() {
   return (
     <>
       <Header />
-      <main>
+      <main className="main">
         <Outlet />
       </main>
       <footer />
