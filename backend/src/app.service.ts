acfd679fb631ca24caa588783bba5e49d2c5c174
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-09-06 17:11:03 +0300

    feat: add env file, add docker compose, add prisma

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly prisma: PrismaService) {}
+
+  async userCount() {
+    const count = await this.prisma.user.count();
+    return {
+      userCount: count,
+    };
   }
 }
